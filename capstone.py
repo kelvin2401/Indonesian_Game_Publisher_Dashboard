98798c75fcb9f3d5f7ec6f4eb22ab2272981f36b
kelvin2401 <kelvin.andersen5@gmail.com>
2024-03-05 15:44:15 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -10,7 +10,7 @@ df = pd.read_pickle('steam_games.pkl')
 
 st.title("Indonesian Game Publisher Analysis")
 
-st.markdown("On January 26, 2024, detikcom, among other news outlets, published a news article about how Indonesia's Ministry of Communication and Information Technology or Kominfo will be blocking video games that are not published by an Indonesian legal body. Kris Antoni, who is the founder of Toge Productions, an Indonesian game developer and publisher company, has tweeted that this regulation is prone to corruption and abuse as well as hinder the Indonesain video game industry. This data dashboard aims to explore the implications of this regulation.")
+st.markdown("On January 26, 2024, detikcom, among other news outlets, published a news article about how Indonesia's Ministry of Communication and Information Technology or Kominfo will be blocking video games that are not published by an Indonesian legal body [1]. Kris Antoni, who is the founder of Toge Productions, an Indonesian game developer and publisher company, has tweeted that this regulation is prone to corruption and abuse as well as hinder the Indonesain video game industry [2]. This data dashboard aims to explore the implications of this regulation.")
 
 mx_games, mx_developers, mx_publishers = st.columns(3)
 
@@ -26,7 +26,7 @@ with mx_publishers:
     
 st.header("Number of Games by Publisher Category and Developer")
 
-st.markdown("Information about Indonesian video games from a popular gaming platform, Steam, are collected, mostly by getting a list of Indonesian game developers from the Indonesian Games Association (AGI) website. Publishers are then categorized into whether they are an Indonesian company or not and whether the Indonesian company is a legal entity or not. A legal entity here means that the company is a Limited Liability Company or PT in Indonesian. From 46 Steam games, 17 of them or 37% would be blocked by Kominfo if they are not republished by an Indonesian legal entity in 2024. These are Indonesian games that are developed by Indonesian game developers.")
+st.markdown("Information about Indonesian video games from a popular gaming platform, Steam, are collected, mostly by getting a list of Indonesian game developers from the Indonesian Games Association (AGI) website [3]. Publishers are then categorized into whether they are an Indonesian company or not and whether the Indonesian company is a legal entity or not. A legal entity here means that the company is a Limited Liability Company or PT in Indonesian. From 46 Steam games, 17 of them or 37% would be blocked by Kominfo if they are not republished by an Indonesian legal entity in 2024. These are Indonesian games that are developed by Indonesian game developers.")
 
 games_count = df['category'].value_counts().reset_index()
 games_count.columns = ['category', 'number_of_games']
@@ -79,7 +79,7 @@ elif option == 'Publisher':
 
 st.dataframe(filtered_df, use_container_width=True, column_config={"_index": None})
 
-st.markdown("In addition, video game publishing regulations from countries all over the world are explored and visualized in a map. This shows that only China and Indonesia require publishers to do extra steps in order to have their games for all ages published. On the other hand, South Korea and countries in the European Union require publishers to have a license to publish video games that contain mature elements.")
+st.markdown("In addition, video game publishing regulations from countries all over the world are explored and visualized in a map. This shows that only China and Indonesia require publishers to do extra steps in order to have their games for all ages published [4]. On the other hand, South Korea requires publishers to have a license to publish video games that contain mature elements [5]. Countries in the European Union do not allow unjustified geo-blocking and certain countries such as Belgium and Austria has banned loot boxes in video games [6][7][8].")
 
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
@@ -105,7 +105,7 @@ countries_red.plot(ax=ax, color='red', edgecolor='black', label='Countries that 
 ax.set_title('Map of Countries with Video Game Publishing Regulations', color='white')  
 ax.axis('off')
 
-orange_legend = plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markersize=5, label='Countries that require license for mature games')
+orange_legend = plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markersize=5, label='Countries that require license for mature games or has additional minor requirements')
 red_legend = plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='red', markersize=5, label='Countries that require license for all games')
 
 ax.legend(handles=[orange_legend, red_legend], loc='lower left', fontsize=8, facecolor='black', edgecolor='white', labelcolor='white')
@@ -115,3 +115,13 @@ fig.patch.set_facecolor('black')
 st.pyplot(fig, use_container_width=False)
 
 st.markdown("The action taken by Kominfo shows that the government wants more control over the gaming industry. However, this decision has made people worry about how it might affect creativity, new ideas, and the overall growth of the gaming sector in Indonesia. The discussion about this rule will probably keep going as people involved look at how it affects things and push for changes to make sure the gaming world in Indonesia can grow well and include everyone.")
+
+st.markdown("Sources:")
+st.markdown("[1] https://inet.detik.com/law-and-policy/d-7162085/publisher-game-wajib-berbadan-hukum-kominfo-kalau-tidak-diblokir")
+st.markdown("[2] https://twitter.com/kerissakti/status/1750916304322875888")
+st.markdown("[3] https://www.agi.or.id")
+st.markdown("[4] https://www.transphere.com/how-to-publish-a-game-in-china/")
+st.markdown("[5] https://www.grac.or.kr/english/")
+st.markdown("[6] https://digital-strategy.ec.europa.eu/en/policies/geoblocking")
+st.markdown("[7] https://online.ucpress.edu/collabra/article/9/1/57641/195100/Breaking-Ban-Belgium-s-Ineffective-Gambling-Law")
+st.markdown("[8] https://www.gamesindustry.biz/ea-loses-minor-fifa-loot-box-legal-case-in-austria")
